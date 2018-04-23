94877485dba15d25b54a53350972df457e284ac7
Ronpiche <vgspm@hotmail.fr>
2018-04-23 21:54:25 +0200

    use the room in the prefab in the game and change the placement of some elements in the canvas

--- Assets/Scripts/CanvasRender/RenderOrchestra.cs
+++ Assets/Scripts/CanvasRender/RenderOrchestra.cs
@@ -27,27 +27,32 @@ public class RenderOrchestra : MonoBehaviour {
         }
         int chorusSize = track.chorusList.Count;
         float width = canvas.GetComponent<RectTransform>().rect.width;
-		float height = canvas.GetComponent<RectTransform>().rect.height;
+        float height = canvas.GetComponent<RectTransform>().rect.height;
         float xInterval = width / (chorusSize + 1);
-		float yInterval = 0.75f * height / (chorusSize + 1);
+        float yInterval = 0.75f * height / (chorusSize + 1);
 
-        int i = 1;
-
+        //int i = 1;
+        int i = 0;
+        float rot =-30f;
+        Vector3[] positionChorus = new Vector3[3];
+        positionChorus[0] = new Vector3(-2600,0,2600);
+        positionChorus[1] = new Vector3(0,0,2600);
+        positionChorus[2] = new Vector3(2600,0,2600);
 		if (track != null)
         {
 			float angleBetweenInstruments = Mathf.PI / chorusSize;
 			float currentAngle = angleBetweenInstruments / 2;
 
 			Debug.Log(canvas.transform.position);
-			
+
             foreach (ChorusScriptable c in track.chorusList)
             {
-
-				//Vector3 pos = new Vector3(xInterval * (i - (chorusSize / 2)), canvas.GetComponent<RectTransform>().rect.height / 2, 0);
-				Vector3 pos = new Vector3(distanceFromCamera * Mathf.Cos(currentAngle), 10, distanceFromCamera * Mathf.Sin(currentAngle));
-                InstantiateChorus(c, pos - canvas.transform.position * 7.0f);
+				        //Vector3 pos = new Vector3(xInterval * (i - (chorusSize / 2)), canvas.GetComponent<RectTransform>().rect.height / 2, 0);
+				        //Vector3 pos = new Vector3(distanceFromCamera * Mathf.Cos(currentAngle), 10, distanceFromCamera * Mathf.Sin(currentAngle));
+                Vector3 pos = positionChorus[i];
+                InstantiateChorus(c, pos /*- canvas.transform.position * 7.0f*/,rot);
                 i++;
-
+                rot +=30;
 				currentAngle += angleBetweenInstruments;
             }
         }
@@ -55,23 +60,23 @@ public class RenderOrchestra : MonoBehaviour {
 
 	void Update ()
     {
-		
+
 	}
 
-    GameObject InstantiateChorus(ChorusScriptable c, Vector3 position)
+    GameObject InstantiateChorus(ChorusScriptable c, Vector3 position, float rot)
     {
         GameObject go = (GameObject) Instantiate(audioSourcePrefab);
         go.name = c.instrumentName;
-		
+
 		CustomAudioSource source = go.GetComponent<CustomAudioSource>();
         source.SetSound(AudioMixer.Instance.Load(c.path));
-		
+
         go.transform.SetParent(this.transform);
         go.AddComponent<Image>();
         go.GetComponent<Image>().sprite = c.instrumentImage;
-        go.transform.localScale = Vector3.one;
-        go.transform.localPosition = 10 * position;
-
+        go.transform.localScale = new Vector3(5,5,5);//Vector3.one;
+        go.transform.localPosition =  position; // *10;
+        go.transform.eulerAngles = new Vector3(0,rot,0);
         orchestraPrefab.AddSource(source);
 
         VolumeDisplay volume = Instantiate(VolumeDislayer,go.transform);
